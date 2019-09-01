8f820db5e30c32093e9225be13f00e7839476762
Raja David Hasugian <rajadavidh@yahoo.com>
2019-09-01 19:06:40 +0700

    add querying dataframe solution

--- course1-intro-to-data-science/week2/quiz-dataframe.py
+++ course1-intro-to-data-science/week2/quiz-dataframe.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+# DataFrame Data structure
 """
 For the purchase records from the pet store, how would you get a list of all items which had been purchased
 (regardless of where they might have been purchased, or by whom)?
@@ -47,3 +49,26 @@ discount['Cost'] = discount['Cost'] * 0.8
 # Solution
 df['Cost'] *= 0.8
 print(df)
+
+
+# Querying DataFrame
+"""
+Write a query to return all of the names of people who bought products worth more than $3.00.
+"""
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+# Your code here
+name = df['Name'][df['Cost'] > 3]
+
+# Solution
+df['Name'][df['Cost']>3]
