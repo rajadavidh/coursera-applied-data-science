afebb125ed8b2caae57186474aa6293524711a59
Raja David Hasugian <rajadavidh@yahoo.com>
2019-09-01 18:38:13 +0700

    add quiz#2 solution

--- course1-intro-to-data-science/week2/quiz-dataframe.py
+++ course1-intro-to-data-science/week2/quiz-dataframe.py
@@ -21,3 +21,29 @@ df.loc[:, 'Item Purchased']
 
 # Solution
 df['Item Purchased']
+
+
+"""
+For the purchase records from the pet store, how would you update the DataFrame, 
+applying a discount of 20% across all the values in the 'Cost' column?
+"""
+
+purchase_1 = pd.Series({'Name': 'Chris',
+                        'Item Purchased': 'Dog Food',
+                        'Cost': 22.50})
+purchase_2 = pd.Series({'Name': 'Kevyn',
+                        'Item Purchased': 'Kitty Litter',
+                        'Cost': 2.50})
+purchase_3 = pd.Series({'Name': 'Vinod',
+                        'Item Purchased': 'Bird Seed',
+                        'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3], index=['Store 1', 'Store 1', 'Store 2'])
+
+# Your answer here
+discount = df.copy()
+discount['Cost'] = discount['Cost'] * 0.8
+
+# Solution
+df['Cost'] *= 0.8
+print(df)
